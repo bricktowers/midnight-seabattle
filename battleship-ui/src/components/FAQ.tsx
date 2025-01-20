1f5db5cd97eb3ea5b2f9959dd8e8d418a816e553
Sergey Kisel <sergey.kisel@bricktowers.io>
2025-01-20 18:05:08 +0100

    Minor changes in readme and styling

--- battleship-ui/src/components/FAQ.tsx
+++ battleship-ui/src/components/FAQ.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from '@mui/material';
 
 export const FAQ: React.FC = () => {
   return (
-    <div style={{ backgroundColor: 'transparent', color: 'cornsilk', marginTop: '32px' }}>
+    <div style={{ backgroundColor: 'transparent', color: 'cornsilk', margin: '32px 32px 32px 32px' }}>
       <Typography align="center" variant="h2" color="primary.dark">
         Frequently Asked Questions
       </Typography>
